634271eda65269a7fbb3bf703741d543a024f368
ShamimKhan2006 <mdshamim200616@gmail.com>
2026-07-20 23:17:31 +0600

    redeply

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   turbopack: {
-//     root: import.meta.dirname,
-//   },
-//   async rewrites() {
-//     return [
-//       {
-//         source: "/api/server/:path*",
-//         destination: "http://localhost:5000/:path*",
-//       },
-//     ];
-//   },
-// };
 
-// export default nextConfig;
 
 
 /** @type {import('next').NextConfig} */
